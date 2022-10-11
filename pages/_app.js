aa859265c5cc81e03d97829fb9edd7ded871b92d
MEDA <118144136+medaworld@users.noreply.github.com>
2022-10-10 21:39:58 -0700

    Setup project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import { Fragment } from 'react';
+import '../styles/globals.css';
+
+import icon from '/public/icon.svg';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <link rel="icon" href={icon.src} />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
