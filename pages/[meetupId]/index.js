ef4e40d379ca62b6d989321aefb90b9238682c5d
MEDA <118144136+medaworld@users.noreply.github.com>
2022-10-11 13:36:29 -0700

    Add meetings detail data from database

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,34 +1,69 @@
 import { Fragment } from 'react';
 import Head from 'next/head';
+import { MongoClient, ObjectId } from 'mongodb';
 
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-const meetupData = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-    address: 'Some Address, 12345 Some City',
-    description: 'This is the first meetup!',
-  },
-];
-
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <Fragment>
       <Head>
-        <title>{meetupData[0].title}</title>
-        <meta name="description" content={meetupData[0].description} />
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
       </Head>
       <MeetupDetail
-        image={meetupData[0].image}
-        title={meetupData[0].title}
-        address={meetupData[0].address}
-        description={meetupData[0].description}
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
       />
     </Fragment>
   );
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(process.env.MONGO_URI);
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
+  return {
+    fallback: 'blocking',
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect(process.env.MONGO_URI);
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
+
+  return {
+    props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
