c4a2bf83d7f1d58fa32aa390e6fa0d637ec224f6
MEDA <118144136+medaworld@users.noreply.github.com>
2022-10-11 13:25:42 -0700

    Connect to mongodb and add POST method

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,13 +1,29 @@
 // our-domain.com/new-meetup
 import { Fragment } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
-async function addMeetupHandler(enteredMeetupData) {
-  console.log(enteredMeetupData);
-}
 function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push('/');
+  }
+
   return (
     <Fragment>
       <Head>
