f41b70776f829d18c6a5fb9d7f3738883d1f9c91
MEDA <118144136+medaworld@users.noreply.github.com>
2022-10-10 21:46:32 -0700

    Start a homepage with dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
 import Head from 'next/head';
+import { Fragment } from 'react';
+
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    address: 'Some Address, 12345 Some City',
+    description: 'This is the first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://images.unsplash.com/photo-1582719508461-905c673771fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80',
+    address: 'Some Address 2, 12345 Some City',
+    description: 'This is the second meetup!',
+  },
+];
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <Fragment>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </Fragment>
+  );
 }
